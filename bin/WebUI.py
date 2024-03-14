e543b3970f7e43541f0e3fe853c76902c2eae512
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-03-15 01:36:49 +0300

    class description

--- bin/WebUI.py
+++ bin/WebUI.py
@@ -2,6 +2,11 @@ from bin.GameEngine import GameEngine
 from bin.GameSettings import GameSettings
 
 class WebUI:
+  '''
+  Provides user interface to game and starts game
+  Site server
+  '''
+
   def __init__(self):
     self.__game = GameEngine()
     self.__game_settings : GameSettings # GameSettings link
