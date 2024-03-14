760f077f7fcac4912cb4001661017edd2affa591
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-03-15 01:12:52 +0300

    GameEngine architecture

--- bin/GameEngine.py
+++ bin/GameEngine.py
@@ -1,20 +1,34 @@
-from GameState import GameState
-from ContentGenerator import ContentGenerator
-from DataStorage import DataStorage
+from bin.GameState import GameState
+from bin.ContentGenerator import ContentGenerator
+from bin.DataStorage import DataStorage
+from bin.GameSettings import GameSettings
 
 class GameEngine:
+
   state = GameState()
   __storage = DataStorage()
   __content_generator = ContentGenerator()
 
   def UserQuery(self, message : str):
+    '''
+    Resolve user game query
+    '''
     pass
   
   def ResetState(self):
+    '''
+    Reset game
+    '''
     pass
 
-  def ChangeSettings(self):
+  def ChangeSettings(self, settings : GameSettings):
+    '''
+    Update settings
+    '''
     pass
 
-  def ChangeCategoryElement(self):
+  def ChangeCategoryElement(self, category : str):
+    '''
+    Change current element by category
+    '''
     pass
