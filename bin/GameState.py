5ca1342228656b8182def0ce8b09684956abf416
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-03-15 01:20:37 +0300

    GameState architecture

--- bin/GameState.py
+++ bin/GameState.py
@@ -1,42 +1,70 @@
 from typing import List, Any # delete Any
-from GameSettings import GameSettings
+from bin.GameSettings import GameSettings
 from bin.Message import Message
 
 class GameState:
+
   __game_settings : GameSettings
   __last_messages : List[Message]
   __system_context : List[Message]
   __bars_info : Any # json!
 
-  def __init__(self):
-    pass
-  
   def GetContextMessages() -> List[Message]:
+    '''
+    Return message for query context
+    '''
     pass
   
   def AddMessage(message : Message):
+    '''
+    Add message to message history
+    '''
     pass
 
   def GetSystemContext() -> List[Message]:
+    '''
+    Return system context messages
+    '''
     pass
 
   def AddSystemContext(message : Message):
+    '''
+    Add message to system context messages
+    '''
     pass
 
   def GetBars() -> Any: # json object!
+    '''
+    Return bars information
+    '''
     pass
 
   def SetBars(bars_info):
+    '''
+    Update bars
+    '''
     pass
 
   def Reset():
+    '''
+    Reset all information to clear game
+    '''
     pass
 
   def SetGameSettings(settings : GameSettings):
+    '''
+    Update game settings
+    '''
     pass
 
-  def SaveToFile():
+  def __SaveToFile():
+    '''
+    Save state to file
+    '''
     pass
 
-  def LoadFromFile():
+  def __LoadFromFile():
+    '''
+    Load state from file
+    '''
     pass
