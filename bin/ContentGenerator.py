7bc170d24d3b44bb0b187f9aad5c8b4810012bf5
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-03-15 01:00:48 +0300

    ContentGenerator architecture

--- bin/ContentGenerator.py
+++ bin/ContentGenerator.py
@@ -1,11 +1,17 @@
 from bin.Message import Message
-from GameState import GameState
+from bin.GameState import GameState
 
 class ContentGenerator:
-  __game_state : GameState # GameState link
+  __game_state : GameState # GameState link!
 
   def UserQuery(self, user_message : str) -> Message:
+    '''
+    Query to chatGPT from user
+    '''
     pass
 
   def SystemQuery(self, system_message : str) -> Message:
+    '''
+    Query from system not for user
+    '''
     pass
