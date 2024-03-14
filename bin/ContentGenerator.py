76423290f1c998575f3dfa404f5d0e344d5f9fb5
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-03-15 01:27:43 +0300

    class description

--- bin/ContentGenerator.py
+++ bin/ContentGenerator.py
@@ -2,6 +2,9 @@ from bin.Message import Message
 from bin.GameState import GameState
 
 class ContentGenerator:
+  '''
+  Provides API for resolving game queries
+  '''
   __game_state : GameState # GameState link!
 
   def UserQuery(self, user_message : str) -> Message:
