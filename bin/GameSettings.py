201e1dd4f4b67d88f39b4906e0878b0af1b6c52e
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-03-22 02:47:40 +0300

    typo fixed

--- bin/GameSettings.py
+++ bin/GameSettings.py
@@ -4,7 +4,7 @@ class GameSettings:
   '''
 
   def __init__(self, message_history_length = 5, context_messages = 5, assistant_instruction = ""):
-    self.message_hystory_length = message_history_length
+    self.message_history_length = message_history_length
     self.context_messages = context_messages
     self.assistant_instruction = assistant_instruction
   
