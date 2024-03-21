a08f1d4faedc3c439fd032ac03be41c2379fed86
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-03-22 01:50:19 +0300

    Default values for compatibility

--- bin/GameSettings.py
+++ bin/GameSettings.py
@@ -3,7 +3,7 @@ class GameSettings:
   Game settings container
   '''
 
-  def __init__(self, message_history_length, context_messages, assistant_instruction):
+  def __init__(self, message_history_length = 5, context_messages = 5, assistant_instruction = ""):
     self.message_hystory_length = message_history_length
     self.context_messages = context_messages
     self.assistant_instruction = assistant_instruction
