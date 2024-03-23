5f55339735f33f4c0c86877ace4436963d99f9ef
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-03-24 02:54:42 +0300

    saving settings compatibility

--- bin/WebUI.py
+++ bin/WebUI.py
@@ -2,7 +2,7 @@
 
 from copy import deepcopy
 from flask import Flask, render_template, request, redirect, flash
-from bin.game_engine import GameEngine
+from game_engine import GameEngine
 
 
 class WebUI:
@@ -14,8 +14,8 @@ class WebUI:
     def __init__(self):
         self.app = Flask(__name__, template_folder='../static')
         self.app.secret_key = 'your_secret_key_here'  # make it secret
-        self._game = GameEngine()
-        self._game_settings = deepcopy(self._game.state._game_settings)
+        self.__game = GameEngine()
+        self.__game_settings = deepcopy(self.__game.state.game_settings)
         self.setup_routes()
 
     def setup_routes(self):
@@ -24,14 +24,14 @@ class WebUI:
         @self.app.route('/')
         def load():
             return render_template('index.html',
-                                   message_history_length=self._game_settings.message_history_length,
-                                   context_messages=self._game_settings.context_messages,
-                                   assistant_instruction=self._game_settings.assistant_instruction)
+                                   history_length=self.__game_settings.history_length,
+                                   context_messages=self.__game_settings.context_messages,
+                                   assistant_instruction=self.__game_settings.assistant_instruction)
 
         @self.app.route('/query', methods=['POST'])
         def user_query_listener():
             '''Listener to user query.'''
-            self._game.user_query(request.form.get('user_query'))
+            self.__game.user_query(request.form.get('user_query'))
 
             flash("Query resolved", "success")
             return redirect('/')
@@ -39,22 +39,21 @@ class WebUI:
         @self.app.route('/reset', methods=['GET'])
         def reset_listener():
             '''Reset button listener.'''
-            self._game.reset_state()
+            self.__game.reset_state()
 
-            flash("Reset", "success")
+            flash("Reset successful", "success")
             return redirect('/')
 
         @self.app.route('/settings_change', methods=['POST'])
         def settings_change_listener():
             '''Settings change listener.'''
             data = request.form
-            self._game_settings.assistant_instruction = data.get(
+            self.__game_settings.assistant_instruction = data.get(
                 'assistant_instruction')
-            self._game_settings.context_messages = data.get('context_messages')
-            self._game_settings.message_history_length = data.get(
-                'message_history_length')
+            self.__game_settings.context_messages = data.get('context_messages')
+            self.__game_settings.history_length = data.get('history_length')
 
-            self._game.settings_change(self._game_settings)
+            self.__game.settings_change(self.__game_settings)
 
             flash("Settings changed", "success")
             return redirect('/')
