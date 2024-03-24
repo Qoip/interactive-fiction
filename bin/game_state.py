988b91b31b3ac45817da431056a5d7d19fc249c7
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-03-24 03:02:36 +0300

    Saving to file functionality

--- bin/game_state.py
+++ bin/game_state.py
@@ -1,7 +1,7 @@
 '''Module containing GameState class.'''
 
 from typing import List, Dict, Any
-from pickle import dump, load
+from json import dump, load, JSONDecodeError
 from game_settings import GameSettings
 from message import Message
 
@@ -9,29 +9,25 @@ from message import Message
 class GameState:
     '''Class responds to saving game current state.'''
 
-    DEFAULT_SETTINGS_PATH = "./data/default_settings.json"
-
-    __game_settings: GameSettings
-    __last_messages: List[Message]
-    __system_context: List[Message]
-    __bars_info: Dict[str, Any]
+    DEFAULT_SETTINGS_PATH = "./data/default_state.json"
 
     def __init__(self):
-        self.__saving_path = "./data/settings.json"
+        self.__game_settings = GameSettings()
+        self.__message_history: List[Message] = []
+        self.__system_context: List[Message] = []
+        self.__bars: Dict[str, Any] = {}
+        self.__saving_path = "./data/state.json"
         self.__load_from_file()
-
-    def __del__(self):
-        self.__save_to_file()
 
     def context_messages(self) -> List[Message]:
         '''Return messages needed for context.'''
-        return self.__last_messages[:self.__game_settings.context_messages]
+        return self.__message_history[:self.__game_settings.context_messages]
 
     def add_message(self, message: Message):
         '''Add message to message history.'''
-        self.__last_messages.append(message)
-        if len(self.__last_messages) > self.__game_settings.history_length:
-            self.__last_messages = self.__last_messages[:self.__game_settings.history_length]
+        self.__message_history.append(message)
+        if len(self.__message_history) > self.__game_settings.history_length:
+            self.__message_history = self.__message_history[:self.__game_settings.history_length]
         self.__save_to_file()
 
     @property
@@ -47,12 +43,12 @@ class GameState:
     @property
     def bars(self) -> Dict[str, Any]:
         '''Return bars information.'''
-        return self.__bars_info
+        return self.__bars
 
     @bars.setter
     def bars(self, bars_info):
         '''Update bars.'''
-        self.__bars_info = bars_info
+        self.__bars = bars_info
         self.__save_to_file()
 
     @property
@@ -63,6 +59,7 @@ class GameState:
     @game_settings.setter
     def game_settings(self, settings: GameSettings):
         '''Update game settings.'''
+        print(type(self.__game_settings))
         self.__game_settings = settings
         self.__save_to_file()
 
@@ -71,10 +68,32 @@ class GameState:
         self.__load_from_file(self.DEFAULT_SETTINGS_PATH)
         self.__save_to_file()
 
+    def to_json(self) -> Dict[str, Any]:
+        '''Converts game state to json object'''
+        return {
+            "game_settings": self.__game_settings.to_json(),
+            "message_history": [message.to_json() for message in self.__message_history],
+            "system_context": [message.to_json() for message in self.__system_context],
+            "bars": self.__bars
+        }
+
+    def from_json(self, json : Dict[str, Any]):
+        '''Loads game state from json object'''
+        try:
+            self.__game_settings.from_json(json["game_settings"])
+            self.__message_history = [Message("", "").from_json(message)
+                                      for message in json["message_history"]] # cringe
+            self.__system_context = [Message("", "").from_json(message)
+                                     for message in json["system_context"]] # cringe
+            self.__bars = json["bars"]
+            self.__save_to_file()
+        except KeyError as exc:
+            raise KeyError(f"JSON decode error:\n{json}") from exc
+
     def __save_to_file(self):
         '''Save state to file.'''
-        with open(self.__saving_path, 'wb') as state_storage:
-            dump(self.__dict__, state_storage)
+        with open(self.__saving_path, 'w', encoding='utf-8') as state_storage:
+            dump(self.to_json(), state_storage, indent=4)
 
     def __load_from_file(self, file_path=None):
         '''Load state from file.'''
@@ -82,18 +101,23 @@ class GameState:
         if file_path is None:
             file_path = self.__saving_path
 
+        data = None
         try:
-            with open(self.__saving_path, 'rb') as state_storage:
-                data = load(state_storage)
+            try:
+                with open(file_path, 'r', encoding='utf-8') as state_storage:
+                    data = load(state_storage)
+            except JSONDecodeError:
+                pass
         except FileNotFoundError as exc:
             file_not_found = True
             if file_path == self.DEFAULT_SETTINGS_PATH:
                 raise FileNotFoundError(f"No default game state file {file_path}.") from exc
 
         if not data or file_not_found:
-            print(f"No data in game state file or file not found: {file_path}")
             if file_path == self.DEFAULT_SETTINGS_PATH:
                 raise ValueError(f"No data in default game state file or not found: {file_path}")
+            print(f"No data in game state file or file not found: {file_path}. \
+Using default game state file.")
             self.__load_from_file(self.DEFAULT_SETTINGS_PATH)
         else:
-            self.__dict__.update(data)
+            self.from_json(data)
