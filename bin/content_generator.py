7ece8ef90330b7ae144b81b69fa17715e3432d3c
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-05-09 01:04:29 +0300

    image generation

--- bin/content_generator.py
+++ bin/content_generator.py
@@ -2,42 +2,52 @@
 
 from message import Message
 from game_state import GameState
+from data_storage import DataStorage
 from openai import OpenAI
 
 
 class ContentGenerator:
     '''Provides API for resolving game queries.'''
 
-    def __init__(self, game_state: GameState):
+    def __init__(self, game_state: GameState, storage: DataStorage):
         self.__game_state: GameState = game_state  # GameState link!
         self.client = OpenAI()  # env OPENAI_API_KEY
+        self.storage = storage
 
     def user_query(self, user_message: str) -> Message:
         '''Query to chatGPT from user.'''
+        self.storage.message_log(Message("user", self.__get_body()))
+        self.storage.message_log(Message("user", user_message))
         text = self.client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4-turbo-2024-04-09",
             messages=[
-                {"role": "system", "content": self.__game_state.game_settings.assistant_instruction},
+                {"role": "user", "content": self.__game_state.game_settings.assistant_instruction},
                 {"role": "user", "content": self.__get_body()},
                 {"role": "user", "content": f"Next user message: {user_message}"}
             ]
         )
-        return Message("assistant", text.choices[0].message.content)
+        answer = Message("assistant", text.choices[0].message.content)
+        self.storage.message_log(answer)
+        return answer
 
-    def system_query(self, system_message: str) -> Message:
-        '''Query from system to category items clarify or storyline intervention.'''
-        text = self.client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": self.__game_state.game_settings.assistant_instruction},
-                {"role": "user", "content": self.__get_body()},
-                {"role": "system", "content": system_message}
-            ]
-        )
-        return Message("assistant", text.choices[0].message.content)
+    def image_generate(self, query: str) -> str:
+        '''Generate image link.'''
+        link = self.client.images.generate(
+            model="dall-e-3",
+            prompt=query,
+            size="1024x1024",
+            quality="standard",
+            n=1
+        ).data[0].url
+        print("Image generated:", link)
+        return link
 
     def __get_body(self) -> str:
         '''Return body of the message.'''
-        return "- context messages:\n" + "\n".join([str(msg) for msg in self.__game_state.context_messages()]) + \
-            "- system context:\n" + "\n".join([str(msg) for msg in self.__game_state.system_context]) + \
-            "- bars:\n" + "\n".join([f"{key}: {value}" for key, value in self.__game_state.bars.items()])
+        body = "- context messages:\n" + "\n".join([str(msg) for msg in self.__game_state.context_messages()]) + \
+            "\n- bars:\n" + "\n".join([f"{key}: {value}" for key, value in self.__game_state.bars.items()])
+        if self.__game_state.system_context:
+            body += "\n- system context:\n" + "\n".join([str(msg) for msg in self.__game_state.system_context])
+        if not self.__game_state.image_link:
+            body += "\nNo location currently, you should add location field to this responce."
+        return body
