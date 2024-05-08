075965e185471ce26a492cad7a9a94b423509fe7
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-05-08 13:24:31 +0300

    init added

--- bin/game_settings.py
+++ bin/game_settings.py
@@ -2,14 +2,16 @@
 
 from typing import Dict, Any
 
+
 class GameSettings:
     '''Game settings container.'''
 
-    history_length : int
-    context_messages : int
-    assistant_instruction : str
+    def __init__(self):
+        self.history_length: int
+        self.context_messages: int
+        self.assistant_instruction: str
 
-    def from_json(self, json : Dict[str, Any]):
+    def from_json(self, json: Dict[str, Any]):
         '''Inits game settings from json object'''
         try:
             self.history_length = json["history_length"]
