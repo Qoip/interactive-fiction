8a972d4df4230e0529e2f5b8a3ece68483559eec
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-05-09 03:28:04 +0300

    reset added

--- bin/data_storage.py
+++ bin/data_storage.py
@@ -1,6 +1,7 @@
 '''Module containing DataStorage class.'''
 
 from message import Message
+import os
 
 
 class DataStorage:
@@ -11,3 +12,9 @@ class DataStorage:
         log_path = './data/log.log'
         with open(log_path, 'a') as file:
             file.write(message.format() + '\n')
+
+    def reset(self):
+        '''Reset data.'''
+        file_path = './data/location.png'
+        if os.path.exists(file_path):
+            os.remove(file_path)
